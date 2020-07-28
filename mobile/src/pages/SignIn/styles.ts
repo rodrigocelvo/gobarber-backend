2dc8e820e33ffbfdc01ae65f6e3a607083e56692
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2020-07-28 14:51:23 -0300

    Update the entire app

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -31,7 +31,7 @@ export const CreateAccountButton = styled.TouchableOpacity`
   left: 0;
   bottom: 0;
   right: 0;
-  background: #312e38;
+  background: #2a2730;
   border-top-width: 1px;
   border-color: #232129;
   padding: 16px 0 ${16 + getBottomSpace()}px;
