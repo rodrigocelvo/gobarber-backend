4c39b55e36c6a1902b87e7c5fffb180c2a0c396d
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2020-08-05 15:10:52 -0300

    feat: Email in production

--- backend/src/modules/users/infra/http/routes/password.routes.ts
+++ backend/src/modules/users/infra/http/routes/password.routes.ts
@@ -8,6 +8,6 @@ const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/forgot', resetPasswordController.create);
+passwordRouter.post('/reset', resetPasswordController.create);
 
 export default passwordRouter;
