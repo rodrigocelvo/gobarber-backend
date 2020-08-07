cf57ddb7ed3b5ababc91ffd343fe577827fc5726
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2020-08-07 14:30:55 -0300

    chore: Added AWS S3 Provider

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,20 +1,45 @@
 import path from 'path';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
-import multer from 'multer';
+
+interface IStorageConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(16).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber',
     },
-  }),
-};
+  },
+} as IStorageConfig;
