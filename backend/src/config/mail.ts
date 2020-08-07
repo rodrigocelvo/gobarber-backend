abdd688c73d9450ee99f13af8955452ea4985809
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2020-08-06 22:25:30 -0300

    chore: SES MailProvider

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'johndoe@gobarber.com.br',
+      name: 'John do GoBarber',
+    },
+  },
+} as IMailConfig;
